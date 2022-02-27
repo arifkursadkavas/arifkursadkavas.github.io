d05282a1a3c5a9ddef52884ecd78df37de6a097d
hashgesh <arifkavas@gmail.com>
2022-02-27 20:40:12 +0100

    error fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-const path = require('path')
-
 module.exports = {
+    webpack: function(config){
+    config.module.rules.push({
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        use: {
+        loader: 'url-loader',
+            options: {
+            limit: 100000,
+            name: '[name].[ext]'
+        }}
+    })
+    return config
+    }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,58 +1,37 @@
-import React from 'react';
-import App from 'next/app';
-import ReactGA from 'react-ga';
-import Sidebar from '../components/Sidebar';
-import { Router } from 'next/dist/client/router';
+import React, { useEffect } from "react";
+import App from "next/app";
+import ReactGA from "react-ga";
+import Sidebar from "../components/Sidebar";
+import { Router } from "next/dist/client/router";
 
-import "../../styles/index.scss";
+import "../../styles/globals.scss";
+import { AppProps } from "next/dist/shared/lib/router/router";
+import Layout from "../components/Layout";
+import Header from "../components/Header";
 
+const HashgeshApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    ReactGA.initialize("UA-108070842-2");
 
-class HashgeshApp extends App {
+    ReactGA.set({ page: "index.html" }); // Update the user's current page
+    ReactGA.pageview("index.html"); // Record a pageview for the given page
 
-    constructor(props) {
-        super(props)
-    }
+    Router.events.on("routeChangeStart", (route: string) => {
+      ReactGA.set({ page: route }); // Update the user's current page
+      ReactGA.pageview(route); // Record a pageview for the given page
+      console.log(route);
+      return true;
+    });
+  });
 
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {};
+  return (
+    <>
+      <Header></Header>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
 
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        return { pageProps };
-    }
-
-    componentDidMount(){
-        ReactGA.initialize('UA-108070842-2');
-
-        ReactGA.set({ page: 'index.html' }); // Update the user's current page
-        ReactGA.pageview('index.html'); // Record a pageview for the given page
-     
-        Router.events.on('routeChangeStart', (route:string)=>{
-            ReactGA.set({ page: route }); // Update the user's current page
-            ReactGA.pageview(route); // Record a pageview for the given page
-            console.log(route)
-            return true
-        })
-    }
-
-    render() {
-        const { Component, pageProps } = this.props;
-        //console.log(this.props);
-        return (
-            <React.Fragment>
-                {/* <Header name='Header' /> */}
-                <Sidebar></Sidebar>
-                <div className='mContentContainer'>
-                    <Component {...pageProps} />
-                </div>
-
-                {/* <Footer name='Footer' /> */}
-            </React.Fragment>
-
-        );
-    }
-}
-
-export default HashgeshApp
+export default HashgeshApp;
