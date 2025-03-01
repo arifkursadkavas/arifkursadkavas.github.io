24194173a4780f8ace07c76e7bcfc8b2e4ee5209
Arif Kavas <arifkavas@gmail.com>
2025-03-01 19:58:44 +0100

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,14 +8,10 @@ import "../../styles/globals.scss";
 import { AppProps } from "next/dist/shared/lib/router/router";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
+import Script from "next/script";
 
 const HashgeshApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
-    ReactGA.initialize("G-PBHF1JX21H");
-
-    ReactGA.set({ page: "index.html" }); // Update the user's current page
-    ReactGA.pageview("index.html"); // Record a pageview for the given page
-
     Router.events.on("routeChangeStart", (route: string) => {
       ReactGA.set({ page: route }); // Update the user's current page
       ReactGA.pageview(route); // Record a pageview for the given page
@@ -26,6 +22,20 @@ const HashgeshApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
+     <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-PBHF1JX21H"
+      />
+
+      <Script strategy="lazyOnload">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PBHF1JX21H');
+         `}
+      </Script>
       <Header></Header>
       <Layout>
         <Component {...pageProps} />
